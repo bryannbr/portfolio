b7fca31dfe8003c32676079900eafd6dd01d6689
Bryan <bryannbr2004@gmail.com>
2025-10-26 00:11:26 -0700

    tweak

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { fetchGitHubData } from './global.js';
 
 
 const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects;
+const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
